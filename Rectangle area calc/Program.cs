01f089131f7bef031205ff1e7b28afcc30e4fe5e
rashishregmi <112930885+rashishregmi@users.noreply.github.com>
2024-04-20 00:05:40 +0545

    Add files via upload
    3

---
+++ Rectangle area calc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Rectangle
+    {
+        public static double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
+
+namespace ProgramNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length, width;
+
+            Console.WriteLine("Rectangle Area Calculator\n");
+
+            Console.Write("Enter length: ");
+            length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter width: ");
+            width = Convert.ToDouble(Console.ReadLine());
+
+            double area = Geometry.Rectangle.CalculateArea(length, width);
+
+            Console.WriteLine($"Area of the rectangle: {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
